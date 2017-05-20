4bf878b7c6de42b3b572921d7011dd38755db40c
Kaitlyn Yang <kaitlynyang2010@gmail.com>
2017-05-20 16:48:06 -0700

    Move

--- src/_123Chess/OneTwoThreeChess.java
+++ src/_123Chess/OneTwoThreeChess.java
@@ -45,7 +45,6 @@ public class OneTwoThreeChess extends javax.swing.JFrame implements MouseListene
 
 	 	Position position;        
 	    ChessBoardPane board_pane;  
-	    JPanel east_pane;
 	    Resource resource = new Resource();
 	    Map<Integer,Image> images = new HashMap<Integer,Image>();
 	    Map<Integer,Icon> icon_images = new HashMap<Integer,Icon>();
@@ -189,7 +188,7 @@ public class OneTwoThreeChess extends javax.swing.JFrame implements MouseListene
         Image animating_image;
         int movingX,movingY,desX,desY,deltaX,deltaY;
         public ChessBoardPane(){
-            setPreferredSize(new Dimension(450,495));
+            setPreferredSize(new Dimension(450, 495));
             setBackground(bg_color);
             addMouseListener(this);
         }
@@ -197,36 +196,38 @@ public class OneTwoThreeChess extends javax.swing.JFrame implements MouseListene
         public void paintComponent(Graphics g){
             if(position.board == null) return;
             super.paintComponent(g);  
-            //g.drawImage(images.get(GameData.MYCHESSMATE),20,36,this);
             Image scaledImage = images.get(GameData.BOARD_IMAGE).getScaledInstance(board_pane.getWidth()-20,board_pane.getHeight()-65,Image.SCALE_SMOOTH);
             g.drawImage(scaledImage,10,55,this);       
             for (int i = 0; i < position.board.length-11; i++) {
                 if (position.board[i] == GameData.ILLEGAL) continue;                                                                
-                int x = i%10;
-                int y = (i-x)/10;
+                int x = i % 10;
+                int y = (i - x) / 10;
                 
                 if (position.board[i] == GameData.EMPTY) continue;
                 
                 if (position.board[i] > 0) {          
                     int piece = position.player1_pieces[position.board[i]].value;
-                    g.drawImage(images.get(piece),x*45,y*45,this);
+                    g.drawImage(images.get(piece), x*45, y*45, this);
                 }else{
                     int piece = position.player2_pieces[-position.board[i]].value;
-                    g.drawImage(images.get(-piece),x*45,y*45,this);
+                    g.drawImage(images.get(-piece), x*45, y*45, this);
                 }               
-            }  
+            } 
+            if(state == GameData.ANIMATING){
+                g.drawImage(animating_image, movingX, movingY, this);
+            }
         }
 
         @Override
         public void mouseClicked(MouseEvent e) {
-            if(state != GameData.PLAYER1_MOVE) return;
+            //if(state != GameData.PLAYER1_MOVE) return;
             int location = boardValue(e.getY())*10+boardValue(e.getX());              
             if(position.board[location] == GameData.ILLEGAL) return;
             if((!piece_selected || position.board[location]>0) && position.board[location] != GameData.EMPTY){
-                if(position.board[location]>0){
+//                if(position.board[location]>0){
                     piece_selected = true;
                     move.source_location = location;
-                }
+//                }
             }else if(piece_selected){
                 piece_selected = false;
                 move.destination = location;     
@@ -284,12 +285,110 @@ public class OneTwoThreeChess extends javax.swing.JFrame implements MouseListene
       loadPieceImages();
       this.board_pane.repaint();
       if (this.is_white) {
-        this.state = 1005;
+        this.state = GameData.PLAYER1_MOVE;
       } else {
-        this.state = 1006;
+        this.state = GameData.PLAYER2_MOVE;
       }
       this.castling = false;
-      this.history_positions.clear();
-      this.history_count = 0;
+      play();
+    }
+    
+    public void play(){
+        Thread t = new Thread(){
+            public void run(){
+                while(true){           
+                    switch(state){
+                        case GameData.PLAYER1_MOVE:    
+                            break;
+                        case GameData.PLAYER2_MOVE:             
+                            break;
+                        case GameData.PREPARE_ANIMATION:
+                            prepareAnimation();
+                            break;
+                        case GameData.ANIMATING:
+                            animate();
+                            break;                        
+                        case GameData.GAME_ENDED: return;
+                    }
+                    try{                        
+                        Thread.sleep(3);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
+    }
+    
+    public void prepareAnimation(){
+        int animating_image_key = 0;
+        if(position.board[move.source_location]>0){
+            animating_image_key = position.player1_pieces[position.board[move.source_location]].value;
+        }else {
+            animating_image_key = -position.player2_pieces[-position.board[move.source_location]].value;
+        }        
+        board_pane.animating_image = images.get(animating_image_key);
+        int x = move.source_location%10;        
+        int y = (move.source_location-x)/10;
+        board_pane.desX = move.destination%10;
+        board_pane.desY = (move.destination-board_pane.desX)/10;
+        int dX = board_pane.desX-x;
+        int dY = board_pane.desY-y;           
+        board_pane.movingX = x*45;
+        board_pane.movingY = y*45;
+        if(Math.abs(dX)>Math.abs(dY)){
+            if(dY == 0){
+                board_pane.deltaX = (dX>0)?1:-1;
+                board_pane.deltaY = 0;
+            }else{
+                board_pane.deltaX = (dX>0)?Math.abs(dX/dY):-(Math.abs(dX/dY));
+                board_pane.deltaY = (dY>0)?1:-1;
+            }
+        }else{
+            if(dX == 0){
+                board_pane.deltaY = (dY>0)?1:-1;
+                board_pane.deltaX = 0;
+            }else{
+                board_pane.deltaX = (dX>0)?1:-1;
+                board_pane.deltaY = (dY>0)?Math.abs(dY/dX):-(Math.abs(dY/dX));
+            }
+        }          
+        state = GameData.ANIMATING;
+    }
+    public void animate(){
+        if (board_pane.movingX == board_pane.desX * 45 && board_pane.movingY == board_pane.desY * 45) {                                           
+            board_pane.repaint();            
+            int source_square = position.board[move.source_location];            
+            if(source_square>0){                
+                state = GameData.PLAYER2_MOVE;                                               
+            }else {
+                if(move.destination > 90 && move.destination<98 
+                        && position.player2_pieces[-source_square].value == Piece.PAWN)
+                    //promoteComputerPawn();
+                state = GameData.PLAYER1_MOVE;
+            }                        
+            position.update(move);       
+            if(source_square>0){
+                if(castling){   
+                    //prepareCastlingAnimation();
+                      state = GameData.PREPARE_ANIMATION;
+                }else if(move.destination > 20 && move.destination < 29 && 
+                        position.player1_pieces[source_square].value == Piece.PAWN){
+                    //promoteHumanPawn();                    
+                }
+            }else{
+//                if (gameEnded(GameData.PLAYER1)) {
+//                    state = GameData.GAME_ENDED;
+//                    return;
+//                }
+            }
+//            if(!castling && state != GameData.PROMOTING) 
+//                newHistoryPosition();
+//            if(castling) castling = false;
+        }
+        board_pane.movingX += board_pane.deltaX;
+        board_pane.movingY += board_pane.deltaY;
+        board_pane.repaint();
     }
 }
